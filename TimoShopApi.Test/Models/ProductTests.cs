6bfdcd28a2ef4083a94b56d66f0a76a4c5340fc4
Timo Hoff <timohoff@live.nl>
2024-02-20 12:23:00 +0100

    introduce TimoShopApi.Test project

---
+++ TimoShopApi.Test/Models/ProductTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using TimoShopApi.Models;
+
+namespace TimoShopApi.Test.Models
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        [DataRow(3, 3)]
+        [DataRow(-1, 0)]
+        [DataRow(6, 5)]
+        public void CartAmount_Sets_IsSet(int inputCartAmount, int expectedCartAmount)
+        {
+            // Arrange
+            var product = new Product
+            {
+                StorageAmount = 5
+            };
+
+            // Act
+            product.CartAmount = inputCartAmount;
+
+            // Assert
+            product.CartAmount.Should().Be(expectedCartAmount);
+        }
+    }
+}
